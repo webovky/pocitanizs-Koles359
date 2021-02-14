d019fcdb4fb6c1169354f6af61eaee52cdf6a3b5
Petr Kolesár <kolesar.p2@seznam.cz>
2021-02-14 19:38:07 +0100

    pokus o synchronizaci zadání a vstupu od uživatele

--- pocitani_zs.py
+++ pocitani_zs.py
@@ -3,6 +3,8 @@
 from os.path import basename, splitext
 import tkinter as tk
 from tkinter import Scale, Canvas, Entry, HORIZONTAL, StringVar, Label, Frame, messagebox, IntVar, Button
+import random
+
 
 class Pocitani(Frame):
 
@@ -21,19 +23,22 @@ class Pocitani(Frame):
         self.master.columnconfigure(1, weight=1500)
         self.master.columnconfigure(2, weight=1500)
         self.rozmisteni()
-
+        self.prvni_cislo()
 
     def rozmisteni(self):
         self.nadpis = Label(text="Vítej v procvičování počítání")
         self.nadpis.grid(row=0, column=1)
 
-        self.cislo1 = Entry(width=15, bd=10, justify="center")
+        self.varCisloa = StringVar()
+        self.cislo1 = Entry(textvariable=self.varCisloa, width=15, bd=10, justify="center")
         self.cislo1.grid(row=1, column=0)
 
-        self.operant = Label(text="")
+        self.varOperant = StringVar()
+        self.operant = Label(textvariable=self.varOperant, justify="center", font=("Helvetica", 18))
         self.operant.grid(row=1, column=1)
 
-        self.cislo2 = Entry(width=15, bd=10, justify="center")
+        self.varCislob = StringVar()
+        self.cislo2 = Entry(textvariable=self.varCislob, width=15, bd=10, justify="center")
         self.cislo2.grid(row=1, column=2)
 
         self.napis = Label(text="Výsledek:")
@@ -42,17 +47,143 @@ class Pocitani(Frame):
         self.vysledek = Entry(width=15, bd=10, justify="center")
         self.vysledek.grid(row=3, column=1)
 
+        self.tlacitko = Button(width=15, bd=10, justify="center", text="ověřit", command=self.prvni_cislo)
+        self.tlacitko.grid(row=4, column=1)
 
+    def prvni_cislo(self):
+        cislo_a = 0
+        cislo_b = 0
+        vstup = 0
 
+        operant = random.choice("+-*/")
+        print(operant)
+        self.varOperant.set(operant)
 
+        if operant == "+":
+            cislo_a = random.randint(0, 101)
+            cislo_b = random.randint(0, 100 - cislo_a)
+            self.varCisloa.set(cislo_a)
+            self.varCislob.set(cislo_b)
+            vstup = int(input())
+            if vstup == (cislo_a + cislo_b):
+                print("správně")
+            else:
+                print("špatně, správný výsledek je ", cislo_a + cislo_b)
 
+        elif operant == "-":
+            cislo_a = random.randint(0, 101)
+            cislo_b = random.randint(0, cislo_a)
+            self.varCisloa.set(cislo_a)
+            self.varCislob.set(cislo_b)
+            vstup = int(input())
+            if vstup == (cislo_a - cislo_b):
+                print("správně")
+            else:
+                print("špatně, správný výsledek je ", cislo_a - cislo_b)
 
+        elif operant == "*":
+            cislo_b = random.randint(2, 11)
+            self.varCislob.set(cislo_b)
 
+            if cislo_b == 2:
+                cislo_a = random.randint(0, 51)
+                self.varCisloa.set(cislo_a)
+                vstup = int(input())
+                if vstup == (cislo_a * cislo_b):
+                    print("správně")
+                else:
+                    print("špatně, správný výsledek je ", cislo_a * cislo_b)
 
+            if cislo_b == 3:
+                cislo_a = random.randint(0, 34)
+                self.varCisloa.set(cislo_a)
+                vstup = int(input())
+                if vstup == (cislo_a * cislo_b):
+                    print("správně")
+                else:
+                    print("špatně, správný výsledek je ", cislo_a * cislo_b)
 
+            if cislo_b == 4:
+                cislo_a = random.randint(0, 26)
+                self.varCisloa.set(cislo_a)
+                vstup = int(input())
+                if vstup == (cislo_a * cislo_b):
+                    print("správně")
+                else:
+                    print("špatně, správný výsledek je ", cislo_a * cislo_b)
+
+            if cislo_b == 5:
+                cislo_a = random.randint(0, 21)
+                self.varCisloa.set(cislo_a)
+                vstup = int(input())
+                if vstup == (cislo_a * cislo_b):
+                    print("správně")
+                else:
+                    print("špatně, správný výsledek je ", cislo_a * cislo_b)
+
+            if cislo_b == 6:
+                cislo_a = random.randint(0, 17)
+                self.varCisloa.set(cislo_a)
+                vstup = int(input())
+                if vstup == (cislo_a * cislo_b):
+                    print("správně")
+                else:
+                    print("špatně, správný výsledek je ", cislo_a * cislo_b)
+
+            if cislo_b == 7:
+                cislo_a = random.randint(0, 15)
+                self.varCisloa.set(cislo_a)
+                vstup = int(input())
+                if vstup == (cislo_a * cislo_b):
+                    print("správně")
+                else:
+                    print("špatně, správný výsledek je ", cislo_a * cislo_b)
+
+            if cislo_b == 8:
+                cislo_a = random.randint(0, 13)
+                self.varCisloa.set(cislo_a)
+                vstup = int(input())
+                if vstup == (cislo_a * cislo_b):
+                    print("správně")
+                else:
+                    print("špatně, správný výsledek je ", cislo_a * cislo_b)
+
+            if cislo_b == 9:
+                cislo_a = random.randint(0, 12)
+                self.varCisloa.set(cislo_a)
+                vstup = int(input())
+                if vstup == (cislo_a * cislo_b):
+                    print("správně")
+                else:
+                    print("špatně, správný výsledek je ", cislo_a * cislo_b)
+
+            if cislo_b == 10:
+                cislo_a = random.randint(0, 11)
+                self.varCisloa.set(cislo_a)
+                vstup = int(input())
+                if vstup == (cislo_a * cislo_b):
+                    print("správně")
+                else:
+                    print("špatně, správný výsledek je ", cislo_a * cislo_b)
+
+        elif operant == "/":
+            cislo_a = random.randint(2, 101)
+            cislo_b = random.randint(2, 11)
+            self.varCisloa.set(cislo_a)
+            self.varCislob.set(cislo_b)
+            if cislo_a % cislo_b == 0:
+                vstup = int(input())
+                if vstup == (cislo_a / cislo_b):
+                    print("správně")
+                else:
+                    print("špatně, správný výsledek je ", cislo_a / cislo_b)
+            else:
+                pass
+
+        else:
+            pass
 
 
 root = tk.Tk()
 app = Pocitani(root)
 root.mainloop()
-
